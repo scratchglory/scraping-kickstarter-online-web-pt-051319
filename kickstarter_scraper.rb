1c20defe1d0e5f4f4baa561ef7fe29944bb730fb
Adella <adellakenn@gmail.com>
2019-06-20 12:02:54 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,14 @@
 # require libraries/modules here
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+
+
+
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
 end
+
