0da45f5746b70ea5a71b9c8f351512263d1676e8
Adella <adellakenn@gmail.com>
2019-06-20 12:44:40 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -17,3 +17,4 @@ end
 # selector == kickstarter.css("li.project.grid_4").first
 # title == project.css("h2.bbcard_name strong a").text        ("Moby Dick: an Oratorio")
 # Image Link == project.css("div.project-thumbnail a img").attribute("src").value
+# description ==  project.css("p.bbcard_blurb").text
